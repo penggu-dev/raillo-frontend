26b28be31f2911ca5dd4e0fabe4e13303eeb0a1d
Yusi <yusi4781@naver.com>
2026-03-11 22:27:12 +0900

    style(button): 공통 버튼 hover, active 디자인 수정 (#8)

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground hover:bg-accent active:bg-primary-active disabled:pointer-events-none disabled:bg-secondary disabled:text-primary-light",
+          "bg-primary text-primary-foreground hover:bg-primary-active disabled:pointer-events-none disabled:bg-secondary disabled:text-primary-light",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
