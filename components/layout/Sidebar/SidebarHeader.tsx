e12c0eb9efb915769abaebf474af7a2997bd192d
Yusi <yusi4781@naver.com>
2026-03-13 03:42:51 +0900

    style(sidebar): 사이드바 UI 디자인 시스템 적용 (#11)

--- components/layout/Sidebar/SidebarHeader.tsx
+++ components/layout/Sidebar/SidebarHeader.tsx
@@ -1,18 +1,19 @@
+import { XIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { X } from "lucide-react";
+import { DrawerClose } from "@/components/ui/drawer";
 
-const SidebarHeader = ({ close }: { close: () => void }) => {
+const SidebarHeader = () => {
   return (
-    <div className="bg-blue-600 text-white p-4 flex items-center justify-between w-full">
-      <h2 className="text-lg font-semibold">카테고리</h2>
-      <Button
-        variant="ghost"
-        size="sm"
-        onClick={() => close()}
-        className="text-white hover:bg-blue-700"
-      >
-        <X className="h-5 w-5" />
-      </Button>
+    <div className="bg-primary text-primary-foreground px-5 flex items-center justify-between w-full h-[72px]">
+      <h2 className="heading-h4">카테고리</h2>
+      <DrawerClose asChild>
+        <Button
+          size="icon"
+          className="text-white hover:bg-blue-700 [&_svg]:size-6"
+        >
+          <XIcon />
+        </Button>
+      </DrawerClose>
     </div>
   );
 };
