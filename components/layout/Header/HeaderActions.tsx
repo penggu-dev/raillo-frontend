c2cd32e4c8c6c6543e9aae8c5dad005b55fb9cb1
Yusi <yusi4781@naver.com>
2026-03-18 01:25:43 +0900

    feat(header): CartBtn 제거 후 예약·승차권 조회 버튼 추가 (#22)
    - 장바구니 버튼 제거.
    - "예약 승차권 조회"(/ticket/reservations),
    - "승차권 조회"(/ticket/purchased) 버튼으로 교체. (#22)

--- components/layout/Header/HeaderActions.tsx
+++ components/layout/Header/HeaderActions.tsx
@@ -1,5 +1,6 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import HeaderAuthBtn from "./HeaderAuthBtn";
-import CartBtn from "./CartBtn";
 import MyPageBtn from "./MyPageBtn";
 import SidebarTrigger from "./SidebarTrigger";
 
@@ -8,7 +9,16 @@ const HeaderActions = () => {
     <div className="flex items-center space-x-4 ml-auto">
       <nav className="hidden md:flex items-center space-x-4">
         <HeaderAuthBtn />
-        <CartBtn />
+        <Link href="/ticket/reservations">
+          <Button variant="ghost" size="sm">
+            예약 승차권 조회
+          </Button>
+        </Link>
+        <Link href="/ticket/purchased">
+          <Button variant="ghost" size="sm">
+            승차권 조회
+          </Button>
+        </Link>
         <MyPageBtn />
         <SidebarTrigger />
       </nav>
