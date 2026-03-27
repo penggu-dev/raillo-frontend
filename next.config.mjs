f2d801e2c13eca051312375b31d53f4b2d16deef
Yusi <yusi4781@naver.com>
2026-03-27 19:21:54 +0900

    refactor: output export 제거 및 불필요한 설정 정리 (#42)

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
-import path from "path"
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias["@" ] = path.resolve("./")
-    return config
-  }
 }
 
 export default nextConfig
