ab1095824d584bf93124929ac2b8c81be1d48081
Yusi <yusi4781@naver.com>
2026-05-16 20:15:35 +0900

    refactor: storageKeys as const 적용 (#50)

--- constants/storageKeys.ts
+++ constants/storageKeys.ts
@@ -2,7 +2,7 @@ export const LOCAL_STORAGE_KEYS = {
   SEARCH_HISTORY: "rail-o-search-history",
   SIGNUP_MEMBER_NUMBER: "signupMemberNo",
   TOSS_CUSTOMER_KEY: "tossCustomerKey",
-};
+} as const;
 
 export const SESSION_STORAGE_KEYS = {
   FOUND_MEMBER_NUMBER: "foundMemberNo",
@@ -10,4 +10,4 @@ export const SESSION_STORAGE_KEYS = {
   PASSWORD_RESET_EMAIL: "tempPasswordEmail",
   IDENTITY_VERIFIED: "emailVerified",
   IDENTITY_VERIFIED_FOR: "emailVerifiedFor",
-};
+} as const;
